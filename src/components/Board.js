c1fecfdd001827905fd2e99dbae01cda496fd308
Benmissi-A <aminebenmissi@hotmail.fr>
2021-04-19 13:04:47 +0200

    board Component clean up

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,7 +4,7 @@ const PublicBoard = (props) => {
   return (
     <>
       <h2 className="text-center display-2 my-3 p-1 bg-secondary text-light">{isLogged ? `Member session` : `Public session`}</h2>
-      <ul>isLogged
+      <ul>
         {crypto.data.map(el => (
           <Crypto crypto = {el}/>
         ))}
