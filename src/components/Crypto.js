9c4a99f6431cf8ce6abe45734ea83c3df14b6b82
Benmissi-A <aminebenmissi@hotmail.fr>
2021-04-19 13:05:07 +0200

    Crypto component in progress

--- src/components/Crypto.js
+++ src/components/Crypto.js
@@ -1,12 +1,22 @@
 const Crypto = (props) => {
   const {crypto} = props
   return(
-    <div key={crypto.id}>
-      <h2>{crypto.name}</h2>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 841.9 595.3" style ={{'width':800+'px','height':200+'px'}}>
+    <div key={crypto.id} className="border border-secondary border rounded col-6 m-auto mb-3">
+      <h2>{`${crypto.name} - ${crypto.name} Rank : ${crypto.rank}`}</h2>
+      <p>{`Price: ${crypto.price_usd} USD / ${crypto.price_btc} BTC`}</p>
+      <p>{`Change: 7d: ${crypto.percent_change_7d}% 24h: ${crypto.percent_change_24h}% 1h: ${crypto.percent_change_1h}%`}</p>
+   
+     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 841.9 595.3" style ={{'width':800+'px','height':200+'px'}}>
         <polyline points="0 0 ,0 200, 800 200,800 0" fill="none" stroke="black"/>
         <polyline points={`0 ${100+Number(crypto.percent_change_7d)},700 ${100+Number(crypto.percent_change_24h)},800 ${100 + Math.abs(crypto.percent_change_1h)}`} fill="none" stroke="green"/>
       </svg>
+     
+      <p>{`Market cap: ${crypto.market_cap_usd} USD`}</p>
+      <p>{`Volume 24: ${crypto.volume24} USD`}</p>
+      <p>{`Volume 24a: ${crypto.volume24a} USD`}</p>
+      <p>{`Csupply: ${crypto.csupply} USD`}</p>
+      <p>{`Tsupply: ${crypto.tsupply} USD`}</p>
+      <p>{`Msupply: ${crypto.msupply} USD`}</p>
     </div>
   )
 }
