9ca33c86dc45c0f71d48ba23fe7d08c00e55e8ab
Benmissi-A <aminebenmissi@hotmail.fr>
2021-04-20 19:10:31 +0200

    update Button in testLogin component

--- src/components/test-components/TestLogin.js
+++ src/components/test-components/TestLogin.js
@@ -1,5 +1,5 @@
 // component temporaire de test avec utilisateurs
-
+import Button from '../Button'
 const TestLogin = (props) => {
 const {isLogged , setIsLogged} = props
   const handleClick = (e) => {
@@ -7,9 +7,11 @@ const {isLogged , setIsLogged} = props
     setIsLogged(!isLogged)
 
   }
+  //<Button type={"button"} value={isLogged ? 'Logout' : 'Login'} btnStyle={ "btn btn-info my-3"} onClick={handleClick}/> 
   return(
     <>
-       <button className="btn btn-info" onClick={handleClick}>{isLogged ? 'Logout' : 'Login'}</button>
+         
+        <button className="btn btn-info" onClick={handleClick}>{isLogged ? 'Logout' : 'Login'}</button>
     </>
   )
 }
