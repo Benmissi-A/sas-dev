9e659573c7f5aed752313d405a923ff031e38077
Benmissi-A <aminebenmissi@hotmail.fr>
2021-04-20 19:08:10 +0200

    add user.js

---
+++ users.js
@@ -0,0 +1,20 @@
+export const users = [
+  { userMail : 'toto@gmail.com',
+    wallet : {
+    cash:1000
+    }
+  },
+  { userMail : 'tata@gmail.com',
+    wallet : {
+    cash:50,
+    btc:0
+    }
+  },
+  { userMail : 'titi@gmail.com',
+    wallet : {
+    cash:1000,
+    btc:0.1,
+    eth:10
+    }
+  }
+]
