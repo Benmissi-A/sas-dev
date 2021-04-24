d7b6104524a67d52190c8e3f1f4a4eae15eb804a
Benmissi-A <aminebenmissi@hotmail.fr>
2021-04-24 16:00:04 +0200

    ModalSign change state to logged on close

--- src/components/modals/ModalSign.js
+++ src/components/modals/ModalSign.js
@@ -1,9 +1,18 @@
 const ModalSign = (props) => {
   const {isLogged , setIsLogged , openLogin , setOpenLogin} = props
+  
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    setIsLogged(!isLogged)
+    setOpenLogin(false)
+  }
+
   if(openLogin){
     return(
-      <div className="modal">
-        <h2>Modal Open </h2>
+      <div className="modal d-block bg-white ">
+        <h2 className="text-dark">Modal Open </h2>
+        <button className="btn btn-info" onClick={handleClick}>Sign up</button>
       </div>
     )
   }else return null
