e56db506020c75b314b1266e74c8ec0ef5403e46
Benmissi-A <aminebenmissi@hotmail.fr>
2021-04-24 15:07:14 +0200

    add ModalSign in progress for the registration

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,14 @@ import TestLogin from './test-components/TestLogin' // temporaire
 
 
 const Header = (props) => {
-  const {isLogged , setIsLogged} = props
+  const {isLogged , setIsLogged , openLogin , setOpenLogin} = props
+  
   
   return (
     <header className="p-5 bg-dark text-center text-light mb-3">
       <h1 className="display-1">S.A.S.</h1>
-      <TestLogin isLogged={isLogged} setIsLogged={setIsLogged}/>
+      <TestLogin isLogged={isLogged} setIsLogged={setIsLogged} openLogin={openLogin} setOpenLogin={setOpenLogin}/>
+
     </header>
   )
 }
