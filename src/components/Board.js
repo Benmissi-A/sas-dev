3c5bc2d50de2ca56f9110b6472a792ec16a67689
Benmissi-A <aminebenmissi@hotmail.fr>
2021-04-24 15:06:08 +0200

    text change now public and logged

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,7 +3,7 @@ const PublicBoard = (props) => {
   const { crypto, isLogged} = props
   return (
     <>
-      <h2 className="text-center display-2 my-3 p-1 bg-secondary text-light">{isLogged ? `Member session` : `Public session`}</h2>
+      <h2 className="text-center display-2 my-3 p-1 bg-secondary text-light">{isLogged ? `Logged` : `Public`}</h2>
       <ul>
         {crypto.data.map(el => (
           <Crypto key={el.id} crypto = {el}/>
