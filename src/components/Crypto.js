30ff3d1e0f64ad409be856536aaac1fff9dfa990
Benmissi-A <aminebenmissi@hotmail.fr>
2021-04-25 02:46:25 +0200

    setCryptoId on props

--- src/components/Crypto.js
+++ src/components/Crypto.js
@@ -1,7 +1,13 @@
 import Button from './Button'
+
 const Crypto = (props) => {
-  const {crypto} = props
-  
+  const {crypto , setOpenCrypto , setCryptoId} = props
+
+  const handleClickModalCrypto = (e) => {  
+    e.preventDefault()
+    setCryptoId(crypto.id)
+    setOpenCrypto(true)
+  }
   return(
     <div key={crypto.id} className="border border-secondary border rounded col-6 m-auto mb-3">
       <h2>{`${crypto.name} - ${crypto.name} Rank : ${crypto.rank}`}</h2>
@@ -19,8 +25,8 @@ const Crypto = (props) => {
       <p>{`Csupply: ${crypto.csupply} USD`}</p>
       <p>{`Tsupply: ${crypto.tsupply} USD`}</p>
       <p>{`Msupply: ${crypto.msupply} USD`}</p>
-      <Button type={"button"} value={'Buy'} btnStyle={ "btn btn-primary my-3"} handleClick={''}/>
+      <Button type={"button"} value={'Buy'} btnStyle={ "btn btn-primary my-3"} handleClick={handleClickModalCrypto}/>
     </div>
-  )
+  ) 
 }
 export default Crypto
