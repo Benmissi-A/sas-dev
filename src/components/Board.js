902b6ffc10293663765b8bd1dd04ddec98cdbe3a
Benmissi-A <aminebenmissi@hotmail.fr>
2021-04-25 02:44:55 +0200

    setCryptoId on props

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,12 +1,14 @@
 import Crypto from './Crypto'
+
 const PublicBoard = (props) => {
-  const { crypto, isLogged} = props
+  const { crypto, isLogged , setOpenCrypto , setCryptoId} = props
+  
   return (
     <>
       <h2 className="text-center display-2 my-3 p-1 bg-secondary text-light">{isLogged ? `Logged` : `Public`}</h2>
       <ul>
         {crypto.data.map(el => (
-          <Crypto key={el.id} crypto = {el}/>
+          <Crypto key={el.id} crypto = {el} setOpenCrypto={setOpenCrypto} setCryptoId={setCryptoId}/>
         ))}
       </ul>
     </>
