af97069023b0cad679266b59bec7d783cce6ddaa
Benmissi-A <aminebenmissi@hotmail.fr>
2021-04-29 18:46:22 +0200

    change ternary to if condition

--- src/components/test-components/TestLogin.js
+++ src/components/test-components/TestLogin.js
@@ -1,14 +1,18 @@
 // component temporaire de test avec utilisateurs
 //import Button from '../Button'
 import ModalSign from '../modals/ModalSign'
-import { users } from '../../users'
+
 
 const TestLogin = (props) => {
   const { isLogged, setIsLogged, openLogin, setOpenLogin, setUser } = props
 
   const handleClickOpenModal = () => {
-    !isLogged ? setOpenLogin(true) : setIsLogged(false)
-    if (isLogged) setUser(users[0])
+
+    if(!isLogged ){setOpenLogin(true) 
+    }else{
+         setIsLogged(false)
+      setUser({})
+    }
     console.log(isLogged)
   }
 
@@ -16,7 +20,7 @@ const TestLogin = (props) => {
     <>
 
       <button className="btn btn-info" onClick={handleClickOpenModal}>{isLogged ? 'Logout' : 'Login'}</button>
-      <ModalSign openLogin={openLogin} setOpenLogin={setOpenLogin} isLogged={isLogged} setIsLogged={setIsLogged} />
+      <ModalSign openLogin={openLogin} setOpenLogin={setOpenLogin} isLogged={isLogged} setIsLogged={setIsLogged} setUser={setUser} />
 
     </>
   )
