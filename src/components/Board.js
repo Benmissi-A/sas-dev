3da37dcc79b7ee55918d67bb2c2a11bbb2da1303
LokiDieKatze <sarah.marques.fr@gmail.com>
2021-04-29 16:42:16 +0200

    test

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,18 +1,14 @@
-import { Buy } from './utils/Buy'
-import { Sell } from './utils/Sell'
-
+import Crypto from './Crypto'
 
 const PublicBoard = (props) => {
-  const { crypto, isLogged } = props
+  const { crypto, isLogged, setOpenCrypto, setCryptoId } = props
+
   return (
     <>
       <h2 className="text-center display-2 my-3 p-1 bg-secondary text-light">{isLogged ? `Logged` : `Public`}</h2>
       <ul>
         {crypto.data.map(el => (
-          <li key={el.id}>{el.name}
-            <button onClick={<Buy price={el.price_usd} isLogged={isLogged} />}>Buy</button>
-            <button onClick={<Sell price={el.price_usd} isLogged={isLogged} />}>Sell</button>
-          </li>
+          <Crypto key={el.id} crypto={el} setOpenCrypto={setOpenCrypto} setCryptoId={setCryptoId} />
         ))}
       </ul>
     </>
